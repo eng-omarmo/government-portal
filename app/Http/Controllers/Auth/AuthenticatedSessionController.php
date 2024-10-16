6ba1cb05bde82841d658f8f297d1ff76f97325d7
eng-omarmoo <omarmoo683@gmail.com>
2024-10-16 10:17:25 +0300

    now user that are in admin with government role can login the portal

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Role;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -25,7 +27,16 @@ class AuthenticatedSessionController extends Controller
     public function store(LoginRequest $request): RedirectResponse
     {
         $request->authenticate();
-
+        $userRoleId = user::where('id', Auth::user()->id)->first()->role_id;
+        if(!$userRoleId){
+            return redirect('/');
+        }
+        $roleName = Role::where('id', $userRoleId)->first()->name;
+        if ($roleName !== 'government') {
+            Auth::logout();
+            $request->session()->invalidate();
+            return redirect('/');
+        }
         $request->session()->regenerate();
 
         return redirect()->intended(route('dashboard', absolute: false));
